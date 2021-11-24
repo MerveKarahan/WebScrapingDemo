74d3a67d077393914b8d8dd6ca95d001e8458a76
Merve Karahan <77892947+MerveKarahan@users.noreply.github.com>
2021-11-24 21:41:14 +0300

    Add project files.

---
+++ WebScraping/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace WebScraping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (; ; )
+            {
+                 
+
+                string data = null;
+                WebClient webl = new WebClient();
+                data = webl.DownloadString("ilgili link");
+
+                Match m = Regex.Match(data, "ilgili div");
+
+                Console.WriteLine("Sleep for 1 seconds."); //sirkülayon devam etsin diye var.
+                Thread.Sleep(1000);
+                Console.WriteLine(m.Groups[5].Value);
+            }
+        }
+    }
+}
---
+++ WebScraping/WhoisLookup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebScraping
+{
+
+    internal class WhoisLookup
+    {
+        public WhoisLookup()
+        {
+            var whois = new WhoisLookup();
+            var response = whois.Lookup("ilgili link");
+        }
+
+        private object Lookup(string v)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
